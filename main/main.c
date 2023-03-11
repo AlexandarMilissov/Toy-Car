423684a087329206a0ae00b31a39e6920ecaf632
amilisov <alex.milissov@gmail.com>
2023-03-11 19:44:24 +0200

    Linker Error Removed

--- main/main.c
+++ main/main.c
@@ -1,14 +1,6 @@
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
+#include "MCM.h"
 
 void app_main(void)
 {
-    int i = 0;
-    while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
+    MCM_Init();
 }
